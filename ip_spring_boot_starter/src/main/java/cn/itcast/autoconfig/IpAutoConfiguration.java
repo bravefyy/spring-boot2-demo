d3251c69ff5cccd15c13e6ecfead7a2171495170
fanyy <fanyy@gmail.com>
2025-02-28 15:39:43 +0800

    init code commit

---
+++ ip_spring_boot_starter/src/main/java/cn/itcast/autoconfig/IpAutoConfiguration.java
@@ -0,0 +1,18 @@
+package cn.itcast.autoconfig;
+
+import cn.itcast.properties.IpProperties;
+import cn.itcast.service.IpCountService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+//@EnableConfigurationProperties(IpProperties.class)
+@Import(IpProperties.class)
+public class IpAutoConfiguration {
+
+    @Bean
+    public IpCountService ipCountService(){
+        return new IpCountService();
+    }
+}
